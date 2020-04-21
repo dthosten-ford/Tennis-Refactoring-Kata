bcbf68edb1da4439347ee7a5f8ba6de844e3cdf7
Dustin Thostenson <sourcetree@delta3consulting.com>
2020-04-21 10:57:30 -0500

    Create a Player struct

--- swift/Tennis/TennisGame1.swift
+++ swift/Tennis/TennisGame1.swift
@@ -1,34 +1,35 @@
 import Foundation
 
+struct Player {
+    let name: String
+    var score: Int
+}
+
 class TennisGame1: TennisGame {
-    private let player1: String
-    private let player2: String
-    private var score1: Int
-    private var score2: Int
+    private var playerOne: Player
+    private var playerTwo: Player
     
     required init(player1: String, player2: String) {
-        self.player1 = player1
-        self.player2 = player2
-        self.score1 = 0
-        self.score2 = 0
+        self.playerOne = Player(name: player1, score: 0)
+        self.playerTwo = Player(name: player2, score: 0)
     }
     
     func wonPoint(_ playerName: String) {
-        if playerName == player1 {
-            score1 += 1
+        if playerName == playerOne.name {
+            playerOne.score += 1
         } else {
-            score2 += 1
+            playerTwo.score += 1
         }
     }
     
-    var scoreIsTied: Bool { score1 == score2 }
+    var scoreIsTied: Bool { playerOne.score == playerTwo.score }
     
     fileprivate func playerIsEligibleToWin(score1: Int, score2: Int) -> Bool {
-        return score1>=4 || score2>=4
+        return playerOne.score >= 4 || playerTwo.score >= 4
     }
     
     fileprivate func calculateScore() -> String {
-        return "\(scoreRepresentator(score1))-\(scoreRepresentator(score2))"
+        return "\(scoreRepresentator(playerOne.score))-\(scoreRepresentator(playerTwo.score))"
     }
     
     fileprivate func scoreRepresentator(_ score: Int) -> String {
@@ -56,7 +57,7 @@ class TennisGame1: TennisGame {
     }
     
     fileprivate func calculateAdvantageorWin() -> String {
-        let minusResult = score1-score2
+        let minusResult = playerOne.score - playerTwo.score
         if minusResult==1 {
             return "Advantage player1"
         } else if minusResult  == -1 {
@@ -69,9 +70,9 @@ class TennisGame1: TennisGame {
     
     var score: String? {
         if scoreIsTied {
-            return calculateTiedScore(score1)
+            return calculateTiedScore(playerOne.score)
         }
-        else if playerIsEligibleToWin(score1: score1, score2: score2) {
+        else if playerIsEligibleToWin(score1: playerOne.score, score2: playerTwo.score) {
             return calculateAdvantageorWin()
         }
         else {
