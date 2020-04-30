8acde98b903281dc15f2d93d74308de64638615a
Dustin Thostenson <sourcetree@delta3consulting.com>
2020-04-30 11:36:47 -0500

    Update TennisGame1.kt
    simplifying code to smaller, readable methods

--- kotlin/src/main/kotlin/TennisGame1.kt
+++ kotlin/src/main/kotlin/TennisGame1.kt
@@ -2,6 +2,10 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
 
     private var m_score1: Int = 0
     private var m_score2: Int = 0
+    private var Love = "Love"
+    private val Fifteen = "Fifteen"
+    private val Thirty = "Thirty"
+    private val forty = "Forty"
 
     override fun wonPoint(playerName: String) {
         if (playerName === "player1")
@@ -11,41 +15,64 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
     }
 
     override fun getScore(): String {
-        var score = ""
-        var tempScore = 0
-        if (m_score1 == m_score2) {
-            when (m_score1) {
-                0 -> score = "Love-All"
-                1 -> score = "Fifteen-All"
-                2 -> score = "Thirty-All"
-                else -> score = "Deuce"
-            }
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
-            val minusResult = m_score1 - m_score2
-            if (minusResult == 1)
-                score = "Advantage player1"
-            else if (minusResult == -1)
-                score = "Advantage player2"
-            else if (minusResult >= 2)
-                score = "Win for player1"
-            else
-                score = "Win for player2"
+
+        if (playersHaveSamePoints()) {
+            return getTieScore(":")
+        } else if (chechickScoreGreaterThan4()) {
+            return getScoreIfNotEqual("")
         } else {
-            for (i in 1..2) {
-                if (i == 1)
-                    tempScore = m_score1
-                else {
-                    score += "-"
-                    tempScore = m_score2
-                }
-                when (tempScore) {
-                    0 -> score += "Love"
-                    1 -> score += "Fifteen"
-                    2 -> score += "Thirty"
-                    3 -> score += "Forty"
-                }
+            return getTempScore("")
+        }
+
+    }
+
+    private fun getTempScore(score: String): String {
+        var tempScore1 = 0
+        var score1 = score
+        for (i in 1..2) {
+            if (i == 1)
+                tempScore1 = m_score1
+            else {
+                score1 += "-"
+                tempScore1 = m_score2
+            }
+
+            when (tempScore1) {
+                0 -> score1 += Love
+                1 -> score1 += Fifteen
+                2 -> score1 += Thirty
+                3 -> score1 += forty
             }
         }
-        return score
+        return score1
     }
+
+    private fun getScoreIfNotEqual(score: String): String {
+        var score1 = score
+        val minusResult = m_score1 - m_score2
+        if (minusResult == 1)
+            score1 = "Advantage player1"
+        else if (minusResult == -1)
+            score1 = "Advantage player2"
+        else if (minusResult >= 2)
+            score1 = "Win for player1"
+        else
+            score1 = "Win for player2"
+        return score1
+    }
+
+    private fun chechickScoreGreaterThan4() = m_score1 >= 4 || m_score2 >= 4
+
+    private fun getTieScore(score: String): String {
+        var score1 = score
+        when (m_score1) {
+            0 -> score1 = "Love-All"
+            1 -> score1 = "Fifteen-All"
+            2 -> score1 = "Thirty-All"
+            else -> score1 = "Deuce"
+        }
+        return score1
+    }
+
+    private fun playersHaveSamePoints() = m_score1 == m_score2
 }
