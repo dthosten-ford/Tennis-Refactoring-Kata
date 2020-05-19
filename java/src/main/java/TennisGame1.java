1626122819af454a082d3f69d0888553c07e9dd6
Dustin Thostenson <sourcetree@delta3consulting.com>
2020-05-19 10:33:53 -0500

    Update TennisGame1.java
    Migrated getAdvantage method with player class.

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,7 +1,7 @@
+import static java.lang.Math.abs;
+
 public class TennisGame1 implements TennisGame {
-    
-    private int playerOneScore = 0;
-    private int playerTwoScore = 0;
+
     static String[] scoreList = {"Love", "Fifteen", "Thirty", "Forty"};
 
     private Player player1;
@@ -16,18 +16,19 @@ public class TennisGame1 implements TennisGame {
     //Strategy pattern.
     
     public void wonPoint(String playerName) {
-        if (playerName == player1.name)
-            playerOneScore += 1;
+        if (playerName == player1.name) {
+            player1.score += 1;
+        }
         else
-            playerTwoScore += 1;
+            player2.score += 1;
     }
 
     public String getScore() {
-        if (playerOneScore == playerTwoScore)
-            return getTieResult(playerOneScore);
-        else if (playerOneScore >=4 || playerTwoScore >=4)
-            return getWinOrAdvantageResult(playerOneScore, playerTwoScore);
-        return getGameResult(playerOneScore, playerTwoScore);
+        if (player1.score == player2.score)
+            return getTieResult(player1.score);
+        else if (player1.score >=4 || player2.score >=4)
+            return getWinOrAdvantageResult(player1, player2);
+        return getGameResult(player1.score, player2.score);
     }
 
     private String getGameResult(int playerOneScore, int playerTwoScore) {
@@ -35,23 +36,19 @@ public class TennisGame1 implements TennisGame {
     }
     
 
-    private String getWinOrAdvantageResult(int playerOneScore, int playerTwoScore) {
-        String score;
-        //Show advantage score for player with the advantage
-        //show winning score for player who is the winner.
+    private String getAdvantagePlayerName(Player player1, Player player2){
+        return (player1.score > player2.score) ? player1.name : player2.name;
+    }
 
-        int minusResult = playerOneScore - playerTwoScore;
-        /*switch (playerOneScore - playerTwoScore){
-            case 1: return "Advantage player1";
-            case -1: return "Advantage player2";
-            case (2 >=
-            default:
-        }*/
-        if (minusResult==1) score ="Advantage "+player1.name;
-        else if (minusResult ==-1) score ="Advantage " + player2.name;
-        else if (minusResult>=2) score = "Win for " + player1.name;
-        else score ="Win for " + player2.name;
-        return score;
+    private Boolean isAdvantageScore(){
+        int minusResult = player1.score - player2.score;
+        return abs(minusResult)==1;
+    }
+
+    private String getWinOrAdvantageResult(Player player1, Player player2) {
+        if (isAdvantageScore())
+            return "Advantage " + getAdvantagePlayerName(player1, player2);
+        return "Win for " + getAdvantagePlayerName(player1, player2);
     }
 
     private String getTieResult(int score) {
@@ -65,16 +62,8 @@ public class TennisGame1 implements TennisGame {
         private String name;
         private int score;
 
-        public Player(String name) {
+        Player(String name) {
             this.name = name;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public int getScore() {
-            return score;
         }
     }
 }
