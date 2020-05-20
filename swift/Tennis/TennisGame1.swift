6ec6d4355073d56a9408612621c3dc85e32dc428
Dustin Thostenson <sourcetree@delta3consulting.com>
2020-05-20 11:00:22 -0500

    Implemented Strategy pattern 2 different ways.

--- swift/Tennis/TennisGame1.swift
+++ swift/Tennis/TennisGame1.swift
@@ -15,6 +15,8 @@ class TennisGame1: TennisGame {
     private var playerTwo: Player
     private let tennisRules = TennisRules()
     private let scoreFormatter = ScoreFormatter()
+    private let strategies: [RulesAndScores] = [PlayerTied(), PlayerAdvantage() ,RegularPlay()]
+    private let altStrategies: [AltRulesAndScores] = [.playerWon]
     
     required init(player1: String, player2: String) {
         self.playerOne = Player(name: player1, points: 0)
@@ -32,22 +34,90 @@ class TennisGame1: TennisGame {
     }
 
     var score: String? {
-        switch tennisRules.calculateGameState(from: playerOne, playerTwo) {
-        case .playerWon(let winner):
-            return scoreFormatter.winner(winner.name)
-        case .tiedScore(let score):
-             return scoreFormatter.calculateTiedScore(score)
-        case .playerHasAdvantage(let player):
-            return scoreFormatter.formattedMessage(forAdvantagePlayer: player.name)
-        case .regularPlay:
-            return scoreFormatter.calculateRegularScore(playerOnePoints: playerOne.points, playerTwoPoints: playerTwo.points)
+        if let altScore = altStrategies.compactMap { $0.getPossibleScore(playerOne, playerTwo) }.first {
+            return altScore
         }
-        // for each implementation of the RulesAndScores protocol, get the score and return it if it exists
+        return strategies.compactMap { $0.getPossibleScore(playerOne, playerTwo) }.first
     }
 }
 
 protocol RulesAndScores {
+    func IshouldBeUsed()-> Bool
     func getPossibleScore(_ player1: Player, _ player2: Player) -> String?
+}
+
+class PlayerWon: RulesAndScores {
+    func IshouldBeUsed() -> Bool {
+        //If FeatureToggle / Gate is ON
+    }
+    
+    
+    func getPossibleScore(_ player1: Player,
+                          _ player2: Player) -> String? {
+        guard case .playerWon(let player) = TennisRules().calculateGameState(from: player1, player2) else { return nil}
+        return ScoreFormatter().winner(player.name)
+    }
+}
+class PlayerWon2021: RulesAndScores {
+    func IshouldBeUsed() -> Bool {
+        //If FeatureToggle / Gate is OFF
+    }
+    
+    
+    func getPossibleScore(_ player1: Player,
+                          _ player2: Player) -> String? {
+        guard case .playerWon(let player) = TennisRules().calculateGameState(from: player1, player2) else { return nil}
+        return ScoreFormatter().winner(player.name)
+    }
+}
+
+class PlayerTied: RulesAndScores {
+    func IshouldBeUsed() -> Bool {
+        <#code#>
+    }
+    
+    
+    func getPossibleScore(_ player1: Player,
+                          _ player2: Player) -> String? {
+        guard case .tiedScore(let score) = TennisRules().calculateGameState(from: player1, player2) else { return nil}
+        return ScoreFormatter().calculateTiedScore(score)
+    }
+}
+
+class PlayerAdvantage: RulesAndScores {
+    func IshouldBeUsed() -> Bool {
+        <#code#>
+    }
+    
+    
+    func getPossibleScore(_ player1: Player,
+                          _ player2: Player) -> String? {
+        guard case .playerHasAdvantage(let player) = TennisRules().calculateGameState(from: player1, player2) else { return nil}
+        return ScoreFormatter().formattedMessage(forAdvantagePlayer: player.name)
+    }
+}
+
+class RegularPlay: RulesAndScores {
+    
+    func getPossibleScore(_ player1: Player,
+                          _ player2: Player) -> String? {
+        return ScoreFormatter().calculateRegularScore(playerOnePoints: player1.points, playerTwoPoints: player2.points)
+    }
+}
+
+struct AltRulesAndScores {
+    private let rule: (Player, Player) -> String?
+
+    func getPossibleScore(_ player1: Player, _ player2: Player) -> String? {
+        rule(player1, player2)
+    }
+
+    static var playerWon: AltRulesAndScores {
+        return AltRulesAndScores { p1, p2 -> String? in
+            guard case .playerWon(let player) = TennisRules().calculateGameState(from: p1, p2) else { return nil}
+            return ScoreFormatter().winner(player.name)
+        }
+    }
 }
 
 class TennisRules {
@@ -63,12 +133,13 @@ class TennisRules {
         if let winner = getWinner(from: player1, player2) {
             return .playerWon(winner)
         }
-        
+        //if Gate ....
         if scoreIsTied(score1: player1.points, score2: player2.points) {
             return .tiedScore(player1.points)
         }
         
         if let player = getAdvantage(from: player1, player2) {
+            //f gate ...
             return .playerHasAdvantage(player)
         }
         return .regularPlay
