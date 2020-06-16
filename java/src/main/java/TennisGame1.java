8e93bcffc6f9b887a64f591b0752ffac13fa98ba
Dustin Thostenson <sourcetree@delta3consulting.com>
2020-06-16 08:39:16 -0500

    first day of tennis kata 2

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,8 +1,8 @@
 
 public class TennisGame1 implements TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private int player1Score = 0;
+    private int player2Score = 0;
     private String player1Name;
     private String player2Name;
 
@@ -12,65 +12,73 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
+        if (playerName.equals("player1"))
+            player1Score += 1;
         else
-            m_score2 += 1;
+            player2Score += 1;
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+        if (isTieScore(player1Score, player2Score)) {
+            return getTieScore(player1Score);
+        } else if (isAdvantageScenario()) {
+            return getScoreForAdvantageScenario(player1Score - player2Score);
+        } else {
+            return getCurrentScore(player1Score) + "-" + getCurrentScore(player2Score);
         }
-        else if (m_score1>=4 || m_score2>=4)
+    }
+
+    private String getCurrentScore(int tempScore) {
+        switch(tempScore)
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            case 3:
+                return "Forty";
+            default:
+                return "";
         }
-        else
+    }
+
+    private String getScoreForAdvantageScenario(int pointDeference) {
+        String score;
+        if (pointDeference==1) score ="Advantage player1";
+        else if (pointDeference ==-1) score ="Advantage player2";
+        else if (pointDeference>=2) score = "Win for player1";
+        else score ="Win for player2";
+        return score;
+    }
+
+    private boolean isAdvantageScenario() {
+        return player1Score >=4 || player2Score >=4;
+    }
+
+    private String getTieScore(int points) {
+        String score;
+        switch (points)
         {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+            case 0:
+                    score = "Love-All";
+                break;
+            case 1:
+                    score = "Fifteen-All";
+                break;
+            case 2:
+                    score = "Thirty-All";
+                break;
+            default:
+                    score = "Deuce";
+                break;
+
         }
         return score;
+    }
+
+    private boolean isTieScore(int m_score1, int m_score2) {
+        return m_score1 == m_score2;
     }
 }
