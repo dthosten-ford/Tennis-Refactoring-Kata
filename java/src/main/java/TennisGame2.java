3879736b5e4ae95b3bec0494494885ab9e38f7cd
Dustin Thostenson <sourcetree@delta3consulting.com>
2020-06-25 12:41:25 -0500

    create player class

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -1,4 +1,8 @@
 
+class Player
+{
+
+}
 public class TennisGame2 implements TennisGame
 {
     public int P1point = 0;
@@ -8,6 +12,8 @@ public class TennisGame2 implements TennisGame
     public String P2res = "";
     private String player1Name;
     private String player2Name;
+
+    private Player Player1 = new Player();
 
     public TennisGame2(String player1Name, String player2Name) {
         this.player1Name = player1Name;
