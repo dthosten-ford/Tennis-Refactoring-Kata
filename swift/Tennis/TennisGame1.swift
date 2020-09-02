ad50a3f9ebdff9f6d28d9bea576156560be3da65
Dustin Thostenson <sourcetree@delta3consulting.com>
2020-09-02 11:03:19 -0500

    simplified the score method

--- swift/Tennis/TennisGame1.swift
+++ swift/Tennis/TennisGame1.swift
@@ -22,64 +22,82 @@ class TennisGame1: TennisGame {
             score2 += 1
         }
     }
+
+    private func isTieScore() -> Bool {
+        return score1 == score2
+    }
+    
+    private func isWinningScore() -> Bool {
+        return score1>=4 || score2>=4
+    }
     
     var score: String? {
-        var score = ""
+        if isTieScore() {
+            return getTieScore()
+        } else if isWinningScore() {
+            return getWinningScore()
+        } else {
+            return updateScore()
+        }
+    }
+
+    func getTieScore() -> String {
+        switch score1
+        {
+        case 0:
+           return "Love-All"
+        case 1:
+           return "Fifteen-All"
+        case 2:
+            return "Thirty-All"
+        default:
+            return "Deuce"
+        }
+    }
+    
+    private func updateScore() -> String {
         var tempScore = 0
-        if score1 == score2
+        var score = ""
+        for i in 1..<3
         {
-            switch score1
-            {
-            case 0:
-                score = "Love-All"
-
-            case 1:
-                score = "Fifteen-All"
-
-            case 2:
-                score = "Thirty-All"
-
-            default:
-                score = "Deuce"
-                
+            if i==1 {
+                tempScore = score1
+            } else {
+                score = "\(score)-"; tempScore = score2
             }
-        }
-        else if score1>=4 || score2>=4
-        {
-            let minusResult = score1-score2
-            if minusResult==1 { score = "Advantage player1" }
-            else if minusResult  == -1 { score = "Advantage player2" }
-            else if minusResult>=2 { score = "Win for player1" }
-            else { score = "Win for player2" }
-        }
-        else
-        {
-            for i in 1..<3
-            {
-                if i==1 { tempScore = score1 }
-                else { score = "\(score)-"; tempScore = score2 }
-                switch tempScore
-                {
-                case 0:
-                    score = "\(score)Love"
-
-                case 1:
-                    score = "\(score)Fifteen"
-
-                case 2:
-                    score = "\(score)Thirty"
-
-                case 3:
-                    score = "\(score)Forty"
-
-                default:
-                    break
-
-                }
-            }
+            score = getReadableScore(value: tempScore, score: score)
         }
         return score
     }
     
+    
+    func getWinningScore() -> String {
+        var score = ""
+        let minusResult = score1-score2
+        if minusResult==1 { score = "Advantage player1" }
+        else if minusResult  == -1 { score = "Advantage player2" }
+        else if minusResult>=2 { score = "Win for player1" }
+        else { score = "Win for player2" }
+        return score
+
+    }
+    func getReadableScore(value: Int, score: String) -> String {
+        switch value {
+        case 0:
+            return "\(score)Love"
+
+        case 1:
+            return "\(score)Fifteen"
+
+        case 2:
+            return "\(score)Thirty"
+
+        case 3:
+            return "\(score)Forty"
+
+        default:
+            return "Error"
+        }
+    }
     
 }
