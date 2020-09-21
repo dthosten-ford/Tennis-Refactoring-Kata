45a40ab841c8d8b62da395023267dd331d1bc076
Dustin Thostenson <sourcetree@delta3consulting.com>
2020-09-21 16:51:06 -0500

    Added builder class to project to test an xmlBuilder concept

---
+++ swift/TennisTests/BuilderTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+
+class BuilderTests: XCTestCase {
+
+    func testXml(){
+//        let builder: XMLParser = XMLParser()
+//        builder.parse("asdf")
+        XCTAssertEqual(1, 2)
+        
+    }
+}
