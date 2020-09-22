eec07f8e475e4a9bde0a9d3f210d39855b5f902d
Dustin Thostenson <sourcetree@delta3consulting.com>
2020-09-22 08:43:07 -0500

    stubbed desired XML with some building examples

---
+++ swift/Tennis/XmlBuilder.swift
@@ -0,0 +1,42 @@
+//
+//  XmlBuilder.swift
+//  TennisTests
+//
+//  Created by Dustin Thostenson on 9/21/20.
+//
+
+import Foundation
+
+class XmlBuilder{
+    
+//    import Cocoa
+//
+//    let node1 = XMLElement(name: "first", stringValue: "Donnie")
+//    let node2 = XMLElement(name: "last", stringValue: "Duck")
+//
+//    let nameNode = XMLElement(name: "name")
+//
+//    nameNode.addChild(node1)
+//    nameNode.addChild(node2)
+//    let root = XMLElement(name: "names")
+//    root.addChild(nameNode)
+//
+//    let document = XMLDocument(rootElement: root)
+//    let xmlData = document.xmlData(options: .nodePrettyPrint)
+//
+//    // swift 3 version:
+//    //let xmlData = document.xmlData(withOptions: Int(XMLNode.Options.nodePrettyPrint.rawValue) )
+//    try? xmlData.write(to: URL(fileURLWithPath: "/tmp/test.xml"))
+//
+//    /*
+//     
+//     cat /tmp/test.xml
+//     
+//     <names>
+//        <name>
+//            <first>Donnie</first>
+//            <last>Duck</last>
+//        </name>
+//     </names>
+//     */
+}
--- swift/TennisTests/BuilderTests.swift
+++ swift/TennisTests/BuilderTests.swift
@@ -2,9 +2,27 @@ import XCTest
 
 class BuilderTests: XCTestCase {
 
-    func testXml(){
-//        let builder: XMLParser = XMLParser()
-//        builder.parse("asdf")
+    //Builder Test outline
+    //1) create code to render XML desired below using Componsite Construction method
+    //2) refactor using BuilderPattern
+    //  hint addChild, addToParent, addAttribute, AddValue
+    func testBuildOneNode(){
+        var expected =
+            "<flavors/>"+
+        "<flavor name='Test-Driven Development'>"+
+        "<requirements>"+
+        "<requirement type='hardware'>"+
+        "1 computer for every 2 participants"+
+        "</requirement>"+
+        "<requirements>"+
+        "<requirement type='software'>"+
+        "IDE"+
+        "</requirement>"+
+        "</requirements>"+
+        "</flavor>"
+        
+        
+        var actual 
         XCTAssertEqual(1, 2)
         
     }
