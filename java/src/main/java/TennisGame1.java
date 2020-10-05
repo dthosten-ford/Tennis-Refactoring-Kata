4c657d88d67cdbbd7eda927acb5c2a4a4f2b7fa6
Dustin Thostenson <sourcetree@delta3consulting.com>
2020-10-05 17:49:17 -0500

    dustin kata and rfc example

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,76 +1,112 @@
+class Player {
+    public String name;
+    public int points;
+}
 
 public class TennisGame1 implements TennisGame {
-    
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private Player playerOne = new Player();
+    private Player playerTwo = new Player();
 
     public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+        this.playerOne.name = player1Name;
+        this.playerTwo.name = player2Name;
     }
+//What keyboard shortcuts do you recall seeing?
+    //Extract functions/methods
+    //testing build - ctrl+shift+F10
+    //selecting code  Ctrl+m or ctrl + shift + M
+    //renaming variable or method name  Shift+F6
+    //Finding refactorings ctrl +shift+alt+T
+    //inline method ctl+alt+N
+    //format code  ctl+alt+L
 
+
+    
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (playerName == "player1") {
+            playerOne.points += 1;
+        } else {
+            playerTwo.points += 1;
+        }
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
+        if (isTie()) return
+                getTieScore();
+        if (isWinningScore()) return
+                getWinningScore();
+        return getInPlayScore("");
+    }
+
+    //    @org.jetbrains.annotations.NotNull
+    private String getInPlayScore(String score) {
+        int tempScore;
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) tempScore = playerOne.points;
+            else {
+                score += "-";
+                tempScore = playerTwo.points;
             }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+            score = fooBar(score, tempScore);
         }
         return score;
+    }
+
+    private String fooBar(String score, int tempScore) {
+        switch (tempScore) {
+            case 0:
+                score += "Love";
+                break;
+            case 1:
+                score += "Fifteen";
+                break;
+            case 2:
+                score += "Thirty";
+                break;
+            case 3:
+                score += "Forty";
+                break;
+        }
+        return score;
+    }
+
+    //    @org.jetbrains.annotations.NotNull
+    private String getWinningScore() {
+        String score;
+        int minusResult = playerOne.points - playerTwo.points;
+        if (minusResult == 1) score = "Advantage player1";
+        else if (minusResult == -1) score = "Advantage player2";
+        else if (minusResult >= 2) score = "Win for player1";
+        else score = "Win for player2";
+        return score;
+    }
+
+    private boolean isWinningScore() {
+        return playerOne.points >= 4 || playerTwo.points >= 4;
+    }
+
+    //    @org.jetbrains.annotations.NotNull
+    private String getTieScore() {
+        String score;
+        switch (playerOne.points) {
+            case 0:
+                score = "Love-All";
+                break;
+            case 1:
+                score = "Fifteen-All";
+                break;
+            case 2:
+                score = "Thirty-All";
+                break;
+            default:
+                score = "Deuce";
+                break;
+
+        }
+        return score;
+    }
+
+    private boolean isTie() {
+        return playerOne.points == playerTwo.points;
     }
 }
