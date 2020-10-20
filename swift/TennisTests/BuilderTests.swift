8b4b67f21cb5658efa9aa29d68eba51ed2ebffd2
Dustin Thostenson <sourcetree@delta3consulting.com>
2020-10-20 08:57:28 -0500

    moved builder tests to appropriate class

---
+++ swift/TennisTests/BuilderTests.swift
@@ -0,0 +1,78 @@
+//
+//  BuilderTests.swift
+//  TennisTests
+//
+//  Created by Dustin Thostenson on 10/20/20.
+//
+
+import XCTest
+
+class BuilderTests: XCTestCase {
+
+    
+    func testBuilder() {
+        let subject = XmlBuilder()
+        let expected =
+        "<names>" +
+        "<name>" +
+        "<first>Donnie</first>" +
+        "<last>Duck</last>" +
+        "</name>" +
+        "</names>"
+        
+        let result = subject.build()
+            .withFirstName(firstName: "Donnie")
+            .withLastname(lastName: "Duck")
+            .toString()
+        XCTAssertEqual(expected, result)
+        
+    }
+    
+    func testNewXMLBuilder() {
+        let subject = XmlBuilder("flavors")
+        let expected = "<flavors></flavors>"
+        let actual = subject.toXml()
+        XCTAssertEqual(expected, actual)
+    }
+    
+    func testAddChild() {
+        let subject = XmlBuilder("anyName")
+        let expected = "<anyName></anyName>"
+        let actual = subject.toXml()
+        XCTAssertEqual(expected, actual)
+    }
+    
+    func testNestedAddChild() {
+        let subject = XmlBuilder("flavors")
+        let expected = "<flavors><flavor></flavor></flavors>"
+        
+        subject.addChild("flavor")
+        let actual = subject.toXml()
+        
+        XCTAssertEqual(expected, actual)
+    }
+    
+    func testNestedAddChildRequirements() {
+        let subject = XmlBuilder("flavors")
+        let expected = "<flavors><flavor><requirements></requirements></flavor></flavors>"
+        
+        subject.addChild("flavor")
+        subject.addChild("requirements")
+        let actual = subject.toXml()
+        
+        XCTAssertEqual(expected, actual)
+    }
+    
+    func testNestedAddChildRequirement() {
+        let expected = "<flavors><flavor><requirements><requirement></requirement></requirements></flavor></flavors>"
+        let subject = XmlBuilder("flavors")
+//        .addChild("flavor")
+//        .addChild("requirements")
+//        .addChild("requirement")
+
+        let actual = subject.toXml()
+        
+        XCTAssertEqual(expected, actual)
+    }
+
+}
